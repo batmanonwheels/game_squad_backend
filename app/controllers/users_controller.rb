c7d77b12be12c1c2407d8a1e2c6c1caa65fe1db6
James Macapagal <j.macapagal@gmail.com>
2022-07-29 10:22:29 -0500

    added User and Session foundation, added Game index

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+    before_action :authorize, only: [:show]
+
+    def create 
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.user_id
+            render json: user, status: :created
+        else
+            render json: {error: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        render json: user
+      end
+
+    private
+    
+    def authorize
+        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+      end
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  resources :games
+  resources :games, only: :index
+
+  # create user signup
+  post "/signup", to: "users#create"
+
+  # show authenticated user 
+  get 'me', to: "users#show"
+
+  # create new session
+  post "/login", to: "sessions#create"
+
+  # destroys session
+  delete "/logout", to: "sessions#destroy"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
