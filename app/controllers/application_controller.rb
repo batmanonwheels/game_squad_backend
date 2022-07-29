6f0c380a3311e943cf80aaf7cbcbe13afe3c757f
James Macapagal <j.macapagal@gmail.com>
2022-07-29 13:01:26 -0500

    fixed CORS errors and got Users to create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    include ActionController::Cookies
+
+    skip_before_action :verify_authenticity_token
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
     before_action :authorize, only: [:show]
-
+    wrap_parameters format: []
     def create 
-        user = User.create(user_params)
+        user = User.create!(user_params)
         if user.valid?
             session[:user_id] = user.user_id
             render json: user, status: :created
         else
             render json: {error: user.errors.full_messages}, status: :unprocessable_entity
         end
+        
     end
 
     def show
