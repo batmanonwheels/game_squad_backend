47aef1558d0743dae46369602bf55bcfc144ccf7
James Macapagal <j.macapagal@gmail.com>
2022-08-02 15:05:15 -0500

    updated seed and added more infra to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :reviews, dependent: :destroy
     has_many :games, through: :reviews
     has_secure_password
+
+    validates :first_name, :last_name, :username,:password, presence: true
 end
