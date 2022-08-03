5d10b3ca0e8da8851806a8986c25c3481a04a8a0
James Macapagal <j.macapagal@gmail.com>
2022-08-03 17:34:12 -0500

    added POST to Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,26 @@ class ReviewsController < ApplicationController
     def index 
         render json: Review.all,status: :ok
     end
+
+    def create 
+        new_review = Review.create!(review_params)
+        render json: new_review, status: :created
+    end
+
+    
+    private 
+
+    def review_params
+        params.permit(
+            :title,
+            :description,
+            :upvotes,
+            :downvotes,
+            :rating,
+            :game_id,
+            :user_id
+        )
+    end
+
+
 end
