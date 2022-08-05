f10b9e7407ce0f00a6f8f3767701a467f07c949a
James Macapagal <j.macapagal@gmail.com>
2022-08-05 11:49:31 -0500

    changed CORS url to heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:4000'
+      origins 'https://obscure-taiga-04714.herokuapp.com'
       resource '*', 
       
       headers: :any, 
